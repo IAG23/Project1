79dd13a561db4b972b6710874c7d1751744980b0
pepep <pepep@Matte>
2023-04-07 20:46:14 -0400

    implemented ()

--- src/demo/Test.java
+++ src/demo/Test.java
@@ -4,5 +4,10 @@ public class Test {
 	
 	public void getInfo() {
 		System.out.println("Pushing code...");
+		
+		System.out.println("Github");
+	}
+	public void add1() {
+		System.out.println(2+3);
 	}
 }
